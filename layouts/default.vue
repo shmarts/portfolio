9c2ca2d1dd97e773e328ee3e301816197e78b457
Martin Rojo <wat.martinrojo@gmail.com>
2019-03-19 10:47:32 +0000

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="app">
+    <header class="header">
+      <nuxt-link class="header__link header__link--name" to="/">Martin Rojo</nuxt-link>
+      <a class="header__link header__link--email" href="mailto:wat.martinrojo@gmail.com">email</a>
+      <a class="header__link header__link--twitter" href="https://twitter.com/martinrojo__" target="__blank">twitter</a>
+      <a class="header__link header__link--instagram" href="https://www.instagram.com/shmvrts/" target="__blank">instagram</a>
+    </header>
+    <nuxt class="view"/>
+    <a class="email-button" href="mailto:wat.martinrojo@gmail.com">email me</a>
+  </div>
+</template>
+
+<style lang="scss">
+.app {
+  max-width: 1000px;
+  margin: 0 auto;
+  padding-bottom: 200px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica,
+    Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+  @media only screen and (max-width: 1200px) {
+    padding: 0 100px 150px;
+    max-width: 100%;
+  }
+  @media only screen and (max-width: 880px) {
+    padding: 0 50px 100px;
+  }
+  @media only screen and (max-width: 600px) {
+    padding: 0 30px 60px;
+  }
+  @media only screen and (max-width: 480px) {
+    padding: 0 20px 50px;
+  }
+}
+
+.header {
+  display: flex;
+  align-items: center;
+  margin-top: 40px;
+  flex-wrap: wrap;
+  &__link {
+    font-size: 14px;
+    font-weight: 700;
+    color: #000;
+    margin-right: 20px;
+    &--{
+      &name {
+        text-transform: uppercase;
+        font-size: 16px;
+        margin-right: 40px;
+        @media only screen and (max-width: 440px) {
+          width: 100%;
+          margin: 0 0 10px 0;
+        }
+      }
+      &email {
+
+      }
+      &twitter {
+        color: #1ca1f2;
+      }
+      &instagram {
+        background: #fec66c; /* Old browsers */
+        background: -moz-linear-gradient(45deg, #fec66c 0%, #f77638 25%, #e33e5f 50%, #bc318f 75%, #6558ca 100%); /* FF3.6-15 */
+        background: -webkit-linear-gradient(45deg, #fec66c 0%,#f77638 25%,#e33e5f 50%,#bc318f 75%,#6558ca 100%); /* Chrome10-25,Safari5.1-6 */
+        background: linear-gradient(45deg, #fec66c 0%,#f77638 25%,#e33e5f 50%,#bc318f 75%,#6558ca 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+      }
+    }
+  }
+}
+
+.view {
+  margin-top: 80px;
+  @media only screen and (max-width: 600px) {
+    margin-top: 40px;
+  }
+}
+
+.email-button {
+  color: #d43030;
+  font-size: 32px;
+  font-weight: 700;
+  position: fixed;
+  bottom: 30px;
+  right: 40px;
+  @media only screen and (max-width: 760px) {
+    display: none;
+  }
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,130 @@
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      clients: [
+        {
+          name: 'CaptainJet & Jetfly',
+          hex: '#8ec9db',
+          role: 'Front End Developer'
+        },
+        {
+          name: 'rchyve',
+          hex: '#d53030',
+          role: 'Founder & Lead Software Engineer'
+        },
+        {
+          name: 'Stunlock Studios',
+          hex: '#e76d2e',
+          role: 'Front End Developer'
+        },
+        {
+          name: 'Dojo Madness',
+          hex: '#7c49fb',
+          role: 'Full Stack Developer'
+        },
+        {
+          name: 'Noka',
+          hex: '#30f6a9',
+          role: 'Full Stack Developer'
+        },
+        {
+          name: 'GRDigital',
+          hex: '#7e7e7e',
+          role: 'Full Stack Developer'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <main class="index">
+    <section class="about">
+      <p class="about__text">
+        Martin — <span style="color: #d53030">Full stack engineer</span> based in London,
+        specialising in developing web apps using modern technologies.
+      </p>
+      <p class="about__text">
+        Founder of <span style="color: #d53030">rchyve</span>, a platform allowing fashion
+        enthusiasts to browse and document archival menswear.
+      </p>
+    </section>
+    <section class="clients">
+      <h2 class="clients__title">Featured clients & projects</h2>
+      <ul class="client-list">
+        <li class="client-list__client" v-for="client in clients" :key="client.name">
+          <span class="client__text" :style="{ color: client.hex }">{{ client.name }}</span>
+          <span class="client__text">{{ client.role }}</span>
+        </li>
+      </ul>
+    </section>
+  </main>
+</template>
+
+<style lang="scss">
+.about__text, .client__text {
+  font-size: 64px;
+  font-weight: 700;
+  line-height: 1.2em;
+  letter-spacing: 0.5px;
+  @media only screen and (max-width: 1000px) {
+    font-size: 48px;
+  }
+  @media only screen and (max-width: 760px) {
+    font-size: 36px;
+  }
+  @media only screen and (max-width: 500px) {
+    font-size: 28px;
+  }
+  @media only screen and (max-width: 400px) {
+    font-size: 24px;
+    letter-spacing: auto;
+  }
+  @media only screen and (max-width: 320px) {
+    font-size: 20px;
+    line-height: 1.1em;
+  }
+}
+
+.about__text {
+  margin-bottom: 1.2em;
+}
+
+.clients {
+  margin-top: 100px;
+  @media only screen and (max-width: 600px) {
+    margin-top: 60px;
+  }
+  &__title {
+    color: rgba(#000, 0.35);
+    text-transform: uppercase;
+    font-size: 16px;
+    font-weight: 700;
+    margin-bottom: 20px;
+    @media only screen and (max-width: 500px) {
+      font-size: 12px;
+      margin-bottom: 10px;
+    }
+  }
+}
+
+.client-list {
+  &__client {
+    margin-bottom: 40px;
+    @media only screen and (max-width: 500px) {
+      margin-bottom: 24px;
+    }
+    @media only screen and (max-width: 320px) {
+      margin-bottom: 16px;
+    }
+  }
+}
+
+.client__text {
+  display: block;
+}
+
+</style>
